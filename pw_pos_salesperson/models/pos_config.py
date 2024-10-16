a5cbeccd8c2cbab3b51cf90371ac22fc01d7d2b4
Benji Codes <89851307+Thaura644@users.noreply.github.com>
2024-10-16 12:57:15 +0300

    Weh

--- pw_pos_salesperson/models/pos_config.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-
-class PosConfig(models.Model):
-    _inherit = 'pos.config'
-
-    allow_salesperson = fields.Boolean(string='Allow Salesperson')
